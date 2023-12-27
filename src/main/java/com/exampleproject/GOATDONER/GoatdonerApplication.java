2b09c4f54dd60305889af5451dc37fb903f465a7
asaaev <niaava1@gmail.com>
2023-12-27 20:51:18 +0200

    Initial commit with plain MVC

---
+++ src/main/java/com/exampleproject/GOATDONER/GoatdonerApplication.java
@@ -0,0 +1,14 @@
+package com.exampleproject.GOATDONER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatdonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatdonerApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/exampleproject/GOATDONER/GoatdonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleproject.GOATDONER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatdonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
